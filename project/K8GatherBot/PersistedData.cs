9156768eb539294a1b51f4b94827a4d347413d79
hprajani <hannu-pekka.rajaniemi@helsinki.fi>
2017-08-30 20:39:58 +0300

    localization refactoring part 1

--- project/K8GatherBot/PersistedData.cs
+++ project/K8GatherBot/PersistedData.cs
@@ -11,8 +11,8 @@ namespace K8GatherBot
         private string fatkidFileName = "fatkid.csv";
         private string highScoreFileName = "highscores.csv";
 
-        private List<DataPair> fatKids = new List<DataPair>();
-        private List<DataPair> highScores = new List<DataPair>();
+        private List<UserData> fatKids = new List<UserData>();
+        private List<UserData> highScores = new List<UserData>();
 
         public PersistedData()
         {
@@ -24,7 +24,7 @@ namespace K8GatherBot
             InitList(highScores, highScoreFileName);
         }
 
-        private void InitList(List<DataPair> data, string fileName)
+        private void InitList(List<UserData> data, string fileName)
         {
             if(!File.Exists(@fileName)) {
                 return;
@@ -34,14 +34,14 @@ namespace K8GatherBot
                 CsvReader csvFile = new CsvReader(fileReader);
                 csvFile.Configuration.HasHeaderRecord = false;
 				csvFile.Read();
-                var records = csvFile.GetRecords<DataPair>();
-                foreach(DataPair pair in records) {
+                var records = csvFile.GetRecords<UserData>();
+                foreach(UserData pair in records) {
                     data.Add(pair);
                 }
             }
 		}
 
-        private void PersistList(List<DataPair> data, string fileName) {
+        private void PersistList(List<UserData> data, string fileName) {
             using (TextWriter writer = new StreamWriter(File.Open(@fileName, FileMode.Create)))
 			{
 				var csv = new CsvWriter(writer);
@@ -65,11 +65,11 @@ namespace K8GatherBot
             PersistList(highScores, highScoreFileName);
         }
 
-        private void Add(List<DataPair> data, string id, string userName) {
-            DataPair entry = data.Find(x => x.id.Equals(id));
+        private void Add(List<UserData> data, string id, string userName) {
+            UserData entry = data.Find(x => x.id.Equals(id));
 
             if(entry == null) {
-                entry = new DataPair(id, userName);
+                entry = new UserData(id, userName);
                 data.Add(entry);
             } else {
                 entry.userName = userName;
@@ -86,8 +86,8 @@ namespace K8GatherBot
             return GetInfo(highScores, userName, response);
         }
 
-        private string GetInfo(List<DataPair> data, string userName, string response) {
-            DataPair entry = data.Find(x => x.userName.Equals(userName));
+        private string GetInfo(List<UserData> data, string userName, string response) {
+            UserData entry = data.Find(x => x.userName.Equals(userName));
             if (entry == null) {
                 return String.Format(response, userName, 0, data.Count, data.Count);
             }
@@ -105,42 +105,42 @@ namespace K8GatherBot
             return GetTop10Info(highScores);
 		}
 
-        private string GetTop10Info(List<DataPair> data) 
+        private string GetTop10Info(List<UserData> data) 
         {
             if(data.Count == 0) {
                 return ":(";
             }
             string list = "";
             for (int i = 0; i < 10 && i != data.Count; i++) {
-                DataPair entry = data[i];
+                UserData entry = data[i];
                 list += i+1 + ". " + entry.userName + " / " + entry.count + "\n";
             }
             return list;
         }
     }
 
-    public class DataPair : IComparable<DataPair>, IEquatable<DataPair> {
+    public class UserData : IComparable<UserData>, IEquatable<UserData> {
         public string id { get; set; }
         public string userName { get; set; }
         public int count { get; set; }
 
-        public DataPair() {
+        public UserData() {
             
         }
 
-        public DataPair(string id, string username) {
+        public UserData(string id, string username) {
             this.id = id;
             this.userName = username;
             this.count = 0;
         }
 
-        public DataPair(string id, string username, int count) {
+        public UserData(string id, string username, int count) {
             this.id = id;
             this.userName = username;
             this.count = count;
         }
 
-        public int CompareTo(DataPair other)
+        public int CompareTo(UserData other)
         {
             if(this.count == other.count) {
                 return string.Compare(this.userName, other.userName, StringComparison.OrdinalIgnoreCase);
@@ -148,7 +148,7 @@ namespace K8GatherBot
             return other.count.CompareTo(this.count);
         }
 
-        public bool Equals(DataPair other)
+        public bool Equals(UserData other)
         {
             return this.id.Equals(other.id);
         }
